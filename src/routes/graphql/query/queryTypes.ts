0b0dce58d5a93e616c5e03b01248c93400a56f31
dmirty.s <d.strizhakov@gmail.com>
2024-03-02 16:42:53 +0300

    feat: add post query

--- src/routes/graphql/query/queryTypes.ts
+++ src/routes/graphql/query/queryTypes.ts
@@ -2,12 +2,14 @@ import { GraphQLObjectType } from 'graphql';
 import { userQuery } from './userQuery/userQuery.js';
 import { profileQuery } from './profileQuery/profileQuery.js';
 import { memberQuery } from './memberQuery/memberQuery.js';
+import { postQuery } from './postQuery/postQuery.js';
 
 export const queryTypes = new GraphQLObjectType({
   name: 'Query',
-  fields: () => ({
+  fields: {
     ...userQuery,
     ...profileQuery,
     ...memberQuery,
-  }),
+    ...postQuery,
+  },
 });
