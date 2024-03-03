a22be580153d9f24faccd00655fec25a89d14191
dmirty.s <d.strizhakov@gmail.com>
2024-03-03 12:28:45 +0300

    feat: add delete user mutations

--- src/routes/graphql/mutations/mutation.ts
+++ src/routes/graphql/mutations/mutation.ts
@@ -5,12 +5,14 @@ import { changePost } from './changePost/changePost.js';
 import { changeUser } from './changeUser/changeUser.js';
 import { createProfile } from './createProfile/createProfile.js';
 import { changeProfile } from './changeProfile/changeProfile.js';
+import { deleteUser } from './deleteUser/deleteUser.js';
 
 export const mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
     ...createUser,
     ...changeUser,
+    ...deleteUser,
     ...createPost,
     ...changePost,
     ...createProfile,
